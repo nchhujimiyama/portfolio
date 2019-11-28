4cd3b9359bc195a9825c00f46bb0288b4ab6a7e3
nchhujimiyama <t___yagi@outlook.jp>
2019-11-29 01:09:00 +0900

    共通パーツ作成

---
+++ wp-content/themes/portfolio/page-main.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Template Name: メインページ
+ */
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <section id="fv">
+            <h2>PORTFOLIO</h2>
+        </section>
+        <section id="blog">
+            <h2>BLOG</h2>
+        </section>
+        <section id="profile">
+            <h2>PROFILE</h2>
+        </section>
+        <section id="works">
+            <h2>WORKS</h2>
+        </section>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ wp-content/themes/portfolio/template-parts/conductor.php
@@ -0,0 +1,11 @@
+<section id="ft-contact">
+    <div class="inner">
+        <h2>CONTACT</h2>
+        <p>
+            Web制作・コンサルティング・システム開発に関するお問い合わせ・<br>
+            お見積もり依頼は、問い合わせフォームより受け付けております。<br>
+            まずはお気軽にご相談ください。
+        </p>
+        <a href="<?= home_url(); ?>/contact/">お問い合わせはこちら</a>
+    </div>
+</section>
