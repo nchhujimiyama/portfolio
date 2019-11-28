e9ee17c5ce6e2d88d894003f7cae2f5f48e7b47e
nchhujimiyama <t___yagi@outlook.jp>
2019-11-29 02:05:39 +0900

    投稿取得関数追加

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -52,3 +52,63 @@ function get_nav($atts) {
 
     return $str;
 }
+
+
+/**
+ * TOP WORKS
+ */
+function get_post_list($posttype, $num) {
+    $str = '';
+    $args = array(
+        'post_type' => $posttype,
+        'post_status' => 'publish',
+        'posts_per_page' => $num
+    );
+    $the_query = new WP_Query($args);
+    if($the_query->have_posts()) {
+        while($the_query->have_posts()) {
+            $the_query->the_post();
+            $str .= '<article>
+                <a href="' . get_the_permalink() . '">
+                    <h3>' . get_the_title() . '</h3>
+                </a>
+            </article>';
+        }
+    } else {
+        $str = '<div class="no-content"><p>準備中です。</p></div>';
+    }
+    wp_reset_postdata();
+
+    return $str;
+}
+
+
+/**
+ * ADD CUSTOM FIELDS
+ */
+add_action( 'admin_menu', 'add_custom_field' );
+function add_custom_field() {
+    add_meta_box( 'custom-EntryData_WorksUrl', 'URL', 'create_EntryData_WorksUrl', 'works', 'normal' );;
+}
+function create_EntryData_WorksUrl() {
+    $keyname = 'EntryData_WorksUrl';
+    global $post;
+    $get_value = get_post_meta( $post->ID, $keyname, true );
+    wp_nonce_field( 'action-' . $keyname, 'nonce-' . $keyname );
+    echo '<input type="text" name="' . $keyname . '" value="' . $get_value . '">';
+}
+add_action( 'save_post', 'save_custom_field' );
+function save_custom_field( $post_id ) {
+    $custom_fields = ['EntryData_WorksUrl'];
+    foreach( $custom_fields as $d ) {
+        if ( isset( $_POST['nonce-' . $d] ) && $_POST['nonce-' . $d] ) {
+            if( check_admin_referer( 'action-' . $d, 'nonce-' . $d ) ) {
+                if( isset( $_POST[$d] ) && $_POST[$d] ) {
+                    update_post_meta( $post_id, $d, $_POST[$d] );
+                } else {
+                    delete_post_meta( $post_id, $d, get_post_meta( $post_id, $d, true ) );
+                }
+            }
+        }
+    }
+}
--- wp-content/themes/portfolio/page-main.php
+++ wp-content/themes/portfolio/page-main.php
@@ -12,12 +12,18 @@ get_header();
         </section>
         <section id="blog">
             <h2>BLOG</h2>
+            <div class="inner">
+                <?= get_post_list('post', 6); ?>
+            </div>
         </section>
         <section id="profile">
             <h2>PROFILE</h2>
         </section>
         <section id="works">
             <h2>WORKS</h2>
+            <div class="inner">
+                <?= get_post_list('works', 6); ?>
+            </div>
         </section>
     </main><!-- #main -->
 </div><!-- #primary -->
